c8ebbaa20b0f9224d42ce57b73f0e77d5314fc93
chralovic <chralovic@protonmail.com>
2020-10-02 00:24:02 +0200

    feat: solve dream team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,5 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function createDreamTeam(members) {
+  let acronym = [];
+  members.forEach(item => {
+    if (typeof item === 'string') {
+      acronym.push(item[0].toUpperCase())
+    } else return false;
 
+  })
+  return acronym.sort().join('');
 };
