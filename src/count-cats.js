7b265fd70e5932a13e6f8c76fc99c2d93d03cefa
chralovic <chralovic@protonmail.com>
2020-10-01 22:43:28 +0200

    init: start algorithms part 1 task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(backyard) {
+  let licznikKotow = 0;
+  let newB = backyard.flat();
+  newB.forEach(item => {
+    if (item === '^^') {
+      licznikKotow++;
+    }
+  })
+  return licznikKotow;
+}
